49e9a6d75cd183873f1e9c112f09406b79ac9f98
Jakub Zárybnický <kubaz@inuits.eu>
2020-11-11 13:23:57 +0100

    Add overlay, use gitignore.nix

--- flake.nix
+++ flake.nix
@@ -1,50 +1,49 @@
 {
   inputs.hakyll = { url = github:jaspervdj/hakyll/master; flake = false; };
+  inputs.gitignore = { url = github:hercules-ci/gitignore.nix/master; flake = false; };
 
-  outputs = { self, nixpkgs, hakyll }: let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  outputs = { self, nixpkgs, gitignore, hakyll }: let
+    inherit (builtins) filterSource;
+    inherit (nixpkgs.lib) flip;
+    inherit (import gitignore { inherit (nixpkgs) lib; }) gitignoreSource;
 
-    filterSite = path: _:
-      let baseName = baseNameOf (toString path); in
-      baseName != "design" &&
-      baseName != "builder" &&
-      baseName != "builder.cabal" &&
-      baseName != "site.hs" &&
-      baseName != "_cache" &&
-      baseName != "_site" &&
-      !(nixpkgs.lib.hasPrefix ".ghc.environment." baseName);
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [ self.overlay ];
+    };
+    src = gitignoreSource ./.;
 
-    filterBuilder = path: _:
-      baseNameOf path == "builder.cabal" ||
-      baseNameOf path == "site.hs";
+  in {
+    overlay = final: prev: {
+      haskellPackages = prev.haskellPackages.extend (prev.haskell.lib.packageSourceOverrides {
+        inherit hakyll;
+        builder = src;
+      });
 
-    hsPackages = pkgs.haskellPackages.extend (pkgs.haskell.lib.packageSourceOverrides {
-      inherit hakyll;
-      zarybnicky-com-builder = builtins.filterSource filterBuilder ./.;
-    });
-
-    zarybnicky-com = pkgs.stdenv.mkDerivation rec {
-      name = "zarybnicky.com";
-      src = builtins.filterSource filterSite ./.;
-      phases = "unpackPhase buildPhase";
-      buildInputs = [ hsPackages.zarybnicky-com-builder ];
-      buildPhase = ''
-        export LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
-        export LANG=en_US.UTF-8
+      zarybnicky-com-builder = final.haskellPackages.builder;
+      zarybnicky-com = final.stdenv.mkDerivation {
+        name = "zarybnicky.com";
+        src = src;
+        phases = "unpackPhase buildPhase";
+        buildInputs = [ final.zarybnicky-com-builder ];
+        buildPhase = ''
+        export LOCALE_A RCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
+        export LANG=en_U  S.UTF-8
         site build
         mkdir -p $out
         cp -r _site/* $out
       '';
+      };
     };
-  in {
-    defaultPackage.x86_64-linux = zarybnicky-com;
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.zarybnicky-com;
     packages.x86_64-linux = {
-      inherit (hsPackages) zarybnicky-com-builder;
-      inherit zarybnicky-com;
+      inherit (pkgs) zarybnicky-com-builder zarybnicky-com;
     };
-    devShell.x86_64-linux = hsPackages.shellFor {
-      packages = p: [ p.zarybnicky-com-builder ];
+    devShell.x86_64-linux = pkgs.haskellPackages.shellFor {
+      packages = p: [ p.builder ];
     };
+
     nixosModule = { config, lib, pkgs, ... }: let
       pkgName = "zarybnicky-com";
       cfg = config.services.${pkgName};
@@ -70,14 +69,12 @@
           recommendedOptimisation = true;
           recommendedProxySettings = true;
 
-          virtualHosts.${cfg.domain}.locations = let
-            pkg = self.packages.x86_64-linux.zarybnicky-com;
-          in {
-            "/".root = pkg;
+          virtualHosts.${cfg.domain}.locations = {
+            "/".root = pkgs.zarybnicky-com;
             "/".extraConfig = ''
               etag off;
               add_header Last-Modified "";
-              add_header etag W/"${builtins.substring 11 32 "${pkg}"}";
+              add_header etag W/"${builtins.substring 11 32 "${pkgs.zarybnicky-com}"}";
             '';
             "/static".root = "/var/www/zarybnicky.com";
           };
